ac2dc5226cef2b1546de4ca64e37d2aabf96630a
beckermr <becker.mr@gmail.com>
2024-10-23 13:30:27 -0500

    chore: version attempt holiday

--- reset_version_attempts.py
+++ reset_version_attempts.py
@@ -1,21 +1,18 @@
 import glob
 import tqdm
-from conda_forge_tick.utils import dump, loads
+from conda_forge_tick.lazy_json_backends import dump, loads
 
 
-all_nodes = glob.glob("./node_attrs/*.json")
+all_nodes = glob.glob("./version_pr_info/**/*.json", recursive=True)
 
 for node_path in tqdm.tqdm(all_nodes):
     with open(node_path, "r") as fp:
         attrs = loads(fp.read())
 
     new_ver = attrs.get("new_version", None)
-    curr_ver = attrs.get("version", None)
     attempts = attrs.get("new_version_attempts", None)
     if (
         new_ver is not None
-        and curr_ver is not None
-        and new_ver != curr_ver
         and attempts is not None
         and attempts.get(new_ver, None) is not None
     ):
