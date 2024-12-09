3f769cb180ae471bb641bafb64ac8c35b44a4624
beckermr <becker.mr@gmail.com>
2024-12-09 08:51:52 -0600

    fix: remove more orphaned files

--- remove_orphaned_files.py
+++ remove_orphaned_files.py
@@ -6,6 +6,28 @@ import subprocess
 from conda_forge_tick.lazy_json_backends import get_sharded_path, LazyJson
 from conda_forge_tick.git_utils import lazy_update_pr_json, github_client
 
+GH = github_client()
+
+
+def _is_pr_duplicated(node, number):
+    repo = GH.get_repo("conda-forge/" + node)
+    orig_pr = repo.get_pull(number)
+
+    node = node.replace("-feedstock", "")
+    with LazyJson(f"node_attrs/{node}.json") as lzj:
+        with lzj["pr_info"] as pr_info:
+            for pr in pr_info["PRed"]:
+                if "PR" not in pr or "number" not in pr["PR"]:
+                    continue
+                _pr = repo.get_pull(int(pr["PR"]["number"]))
+                if _pr.title == orig_pr.title:
+                    if orig_pr.state == "open":
+                        orig_pr.edit(state="closed")
+                    return True
+
+    return False
+
+
 # clean out oprhaned PR json blobs
 print("getting all pr json blobs", flush=True)
 all_pr_json = glob.glob("./pr_json/**/*.json", recursive=True)
@@ -31,8 +53,6 @@ for pr_info in all_pr_info:
 orphaned_pr_json = all_pr_json - found_pr_json
 print(f"Found {len(orphaned_pr_json)} orphaned PR json blobs")
 
-gh = github_client()
-
 for pr_json in orphaned_pr_json:
     with LazyJson(pr_json) as lzj:
         pr_data = lazy_update_pr_json(copy.deepcopy(lzj.data))
@@ -49,7 +69,7 @@ for pr_json in orphaned_pr_json:
                 pth = get_sharded_path(pr_json)
                 subprocess.run(["git", "rm", "-f", pth], check=True)
             else:
-                repo = gh.get_repo("conda-forge/" + pr_data["base"]["repo"]["name"])
+                repo = GH.get_repo("conda-forge/" + pr_data["base"]["repo"]["name"])
                 if repo.archived:
                     print(
                         "can remove %s - repo is archived" % pr_json,
@@ -58,6 +78,14 @@ for pr_json in orphaned_pr_json:
                     pth = get_sharded_path(pr_json)
                     subprocess.run(["git", "rm", "-f", pth], check=True)
                 else:
-                    print("%s: %s" % (pr_data["state"], pr_data["html_url"]), flush=True)
+                    if _is_pr_duplicated(pr_data["base"]["repo"]["name"], pr_data["number"]):
+                        print(
+                            "can remove %s - PR is duplicated" % pr_json,
+                            flush=True,
+                        )
+                        pth = get_sharded_path(pr_json)
+                        subprocess.run(["git", "rm", "-f", pth], check=True)
+                    else:
+                        print("orphaned PR (%s): %s" % (pr_data["state"], pr_data["html_url"]), flush=True)
         else:
             print(f"could not load {pr_json}", flush=True)
