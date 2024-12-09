94ec78611d3f34f6a3f412fa6dc24e22e92a2cc9
beckermr <becker.mr@gmail.com>
2024-12-09 06:35:35 -0600

    fix: remove more orphaned files

--- remove_orphaned_files.py
+++ remove_orphaned_files.py
@@ -4,7 +4,7 @@ import json
 import subprocess
 
 from conda_forge_tick.lazy_json_backends import get_sharded_path, LazyJson
-from conda_forge_tick.git_utils import lazy_update_pr_json
+from conda_forge_tick.git_utils import lazy_update_pr_json, github_client
 
 # clean out oprhaned PR json blobs
 print("getting all pr json blobs", flush=True)
@@ -31,17 +31,33 @@ for pr_info in all_pr_info:
 orphaned_pr_json = all_pr_json - found_pr_json
 print(f"Found {len(orphaned_pr_json)} orphaned PR json blobs")
 
+gh = github_client()
+
 for pr_json in orphaned_pr_json:
     with LazyJson(pr_json) as lzj:
         pr_data = lazy_update_pr_json(copy.deepcopy(lzj.data))
-        if pr_data is not None and pr_data.get("state", None) == "closed":
-            assert pr_data["id"] == int(pr_json.split("/")[-1].split(".")[0]), (
-                pr_data["id"],
-                pr_json,
-            )
-            print(
-                "can remove %s - last modified at %s" % (pr_json, pr_data["Last-Modified"]),
-                flush=True,
-            )
-            pth = get_sharded_path(pr_json)
-            subprocess.run(["git", "rm", "-f", pth], check=True)
+        if pr_data is not None:
+            if pr_data.get("state", None) == "closed":
+                assert pr_data["id"] == int(pr_json.split("/")[-1].split(".")[0]), (
+                    pr_data["id"],
+                    pr_json,
+                )
+                print(
+                    "can remove %s - last modified at %s" % (pr_json, pr_data["Last-Modified"]),
+                    flush=True,
+                )
+                pth = get_sharded_path(pr_json)
+                subprocess.run(["git", "rm", "-f", pth], check=True)
+            else:
+                repo = gh.get_repo("conda-forge/" + pr_data["base"]["repo"]["name"])
+                if repo.archived:
+                    print(
+                        "can remove %s - repo is archived" % pr_json,
+                        flush=True,
+                    )
+                    pth = get_sharded_path(pr_json)
+                    subprocess.run(["git", "rm", "-f", pth], check=True)
+                else:
+                    print("%s: %s" % (pr_data["state"], pr_data["html_url"]), flush=True)
+        else:
+            print(f"could not load {pr_json}", flush=True)
